fdbe2132c25c1dff1cbb9438e1979799b84070ea
Lakshay Hasija <75783120+LakshayHasija@users.noreply.github.com>
2025-09-01 21:21:30 +0530

    Time: 611 ms (61.12%), Space: 17.9 MB (90.81%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -2,8 +2,8 @@ class Solution:
     def minInsertions(self, word1: str) -> int:
         word2=word1[::-1]
         n=len(word1)
-        curr=[0 for i in range(n+1)] #CURR
-        prev=[0 for i in range(n+1)] #PREV
+        curr=[0 for i in range(n+1)]
+        prev=[0 for i in range(n+1)]
         for i in range(1,n+1):
             for j in range(1,n+1):
                 if word1[i-1]==word2[j-1]:
@@ -12,12 +12,4 @@ class Solution:
                     curr[j]=max(curr[j-1],prev[j])
             prev,curr=curr,[0 for i in range(n+1)]
         return n-prev[n]
-        for i in range(1, n+1):
-            prev = 0
-            for j in range(1, n+1):
-                temp = dp[j]   
-                if s[i-1] == rev[j-1]:
-                    dp[j] = 1 + prev
-                else:
-                    dp[j] = max(dp[j], dp[j-1])
-                prev = temp
+        
