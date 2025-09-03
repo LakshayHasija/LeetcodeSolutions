76e5c8d24caebe80a4aaedfbc9c110eb953f7714
Lakshay Hasija <75783120+LakshayHasija@users.noreply.github.com>
2025-09-03 21:59:44 +0530

    Time: 4222 ms (5.1%), Space: 18.2 MB (81.47%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -2,19 +2,19 @@ class Solution:
     def ispalandrom(self,s):
         return s==s[::-1]
     def minCut(self, s: str) -> int:
-        memo={}
+        dp=[-1]*(len(s)+1)
         def solve(i,j):
             if i>=j:
                 return 0
             if self.ispalandrom(s[i:j+1]):
                 return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
+            if dp[i]!=-1:
+                return dp[i]
             mincuts=999999999
             for k in range(i,j):
                 if self.ispalandrom(s[i:k+1]):
                     cuts=solve(k+1,j)+1
                     mincuts=min(mincuts,cuts)
-            memo[(i,j)]=mincuts
+            dp[i]=mincuts
             return mincuts
         return solve(0,len(s)-1)
