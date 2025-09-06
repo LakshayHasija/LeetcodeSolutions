70f4346fe67d9973c2f79d077e4159ad0ba222df
Lakshay Hasija <75783120+LakshayHasija@users.noreply.github.com>
2025-09-07 01:34:12 +0530

    Time: 142 ms (51.55%), Space: 45.1 MB (6.75%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,13 +1,8 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp={}
+        @lru_cache(None) #this is amazing tool from python - DP without needing to store any input just solve the problem in recursion as i used to do beforehand but there is a drawback- You can’t use lists or dictionaries as arguments they must be immutable types like int, str, tuple. 
         def backtracking(index,tempsum):
             if index==len(nums):
                 return 1 if tempsum==target else 0
-            if (index,tempsum) in dp:
-                return dp[(index,tempsum)]
-            positive=backtracking(index+1,tempsum+nums[index])
-            negetive=backtracking(index+1,tempsum-nums[index])
-            dp[(index,tempsum)]=positive+negetive
-            return dp[(index,tempsum)]
+            return backtracking(index+1,tempsum+nums[index])+backtracking(index+1,tempsum-nums[index])
         return backtracking(0,0)
