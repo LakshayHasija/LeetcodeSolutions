438ecf5d816ad586b0d9fed27951a947e8a4cea6
Lakshay Hasija <75783120+LakshayHasija@users.noreply.github.com>
2025-09-08 00:18:46 +0530

    Time: 1 ms (9.59%), Space: 17.9 MB (56.31%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
@@ -4,7 +4,6 @@ class Solution:
             ans=[]
         else:
             ans=[0]
-            n-=1    #n is a even number now
         for i in range(1,n//2+1):
             ans.append(i)
             ans.append(i*-1)
