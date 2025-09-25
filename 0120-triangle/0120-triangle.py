c4c751ec30f46d33fce3bdd14016c382a944affc
Lakshay Hasija <75783120+LakshayHasija@users.noreply.github.com>
2025-09-26 00:29:32 +0530

    Time: 7 ms (33.7%), Space: 19.7 MB (20.17%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,11 +1,14 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         height=len(triangle)
-        @lru_cache(None)
+        dp = [[None] * len(row) for row in triangle]
         def helper(level,idx):
-            if level==height:
-                return 0
-            same=triangle[level][idx]+helper(level+1,idx)
-            notsame=triangle[level][idx+1]+helper(level+1,idx+1)
-            return min(same,notsame)
-        return helper(1,0)+triangle[0][0]
+            if level==height-1:
+                return triangle[level][idx]
+            if dp[level][idx] is not None:
+                return dp[level][idx]
+            same=helper(level+1,idx)
+            notsame=helper(level+1,idx+1)
+            dp[level][idx]=triangle[level][idx]+min(same,notsame)
+            return dp[level][idx]
+        return helper(0,0)
