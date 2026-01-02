ac5eccdcfa8175797e047224cecae5ec893d0732
Lakshay Hasija <75783120+LakshayHasija@users.noreply.github.com>
2026-01-02 17:07:53 +0530

    Time: 7 ms (47.84%), Space: 18.5 MB (84.85%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        n=len(nums)//2
+        freq=Counter(nums)
+        for k,v in freq.items():
+            if v==n:
+                return k
